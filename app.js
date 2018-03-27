56497479c1fa91529ca4e06f54cb0fc00b2d42ed
Brandon Samuel <brandonsamuel72@gmail.com>
2018-03-27 06:59:18 +0000

    added routes, public and views folder with correct files in them and styled landing page

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const authRoutes = require("./routes/index");
 
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressSanitizer());
+//app.use(expressSanitizer());
 //app.use(methodOverride("_method"));
 
-
+app.use(authRoutes);
 
 
 app.listen(process.env.PORT || 8080, function(){
